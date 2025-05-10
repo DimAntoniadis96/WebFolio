f3456347f5e1dae8b08b647bcf769b576f2c5838
DimAntoniadis96 <dim.antoniadis.dev@gmail.com>
2025-05-10 22:42:47 +0300

    Καθαρισμός CSS ::before

---
+++ script.js
@@ -0,0 +1,36 @@
+const typedTextSpan = document.querySelector(".typing-text span");
+const textArray = [
+  " Web Developer",
+  " Software Developer",
+  " Web Designer",
+  " Sound Engineer",
+  " Songwriter"
+];
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (textArray.length) setTimeout(type, 1000);
+});
